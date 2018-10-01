601fdee14dcc145dd864a1c7175c79f53b6f3c21
Damien Bournac <damienbournac@gmail.com>
2018-10-01 17:20:24 +0200

    la suite de la suite de la suite de cette satanée quête

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -11,19 +11,19 @@ use Model;
 
 
 
-
 class ItemController
 {
 
     public function index()
     {
+
         $itemManager = new \Model\ItemManager();
         $items = $itemManager->selectAllItems();
-        return $items;
+        require __DIR__ . '/../View/Item.php';
 
 
     }
 }
-require __DIR__ . '/../View/Item.php';
+
 
 ?>
